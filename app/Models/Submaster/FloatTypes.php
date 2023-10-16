4b8c0335eaa8ffbd952dba0fbd86782166688d2b
patrickDigits <patrickpunzalan@digits.ph>
2023-10-16 18:49:15 +0800

    models

---
+++ app/Models/Submaster/FloatTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Submaster;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FloatTypes extends Model
+{
+    use HasFactory;
+    
+    protected $guarded = [];
+}
