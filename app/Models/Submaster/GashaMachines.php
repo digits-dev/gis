f122032c946f592b698b897a784bbf3dff2fa1fe
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-10-16 16:27:58 +0800

    update name of locations model

--- app/Models/Submaster/GashaMachines.php
+++ app/Models/Submaster/GashaMachines.php
@@ -5,7 +5,7 @@ namespace App\Models\Submaster;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class gasha_machines extends Model
+class GashaMachines extends Model
 {
     use HasFactory;
 }
