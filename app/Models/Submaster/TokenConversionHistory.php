b094b25709f477b324de8d1a9a19a6894ac5325e
Fillinor <fillinorgunio@digits.ph>
2023-10-16 17:01:42 +0800

    create token conversion history model and migration

---
+++ app/Models/Submaster/TokenConversionHistory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Submaster;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TokenConversionHistory extends Model
+{
+    use HasFactory;
+    protected $table = 'token_conversion_histories';
+}
---
+++ database/migrations/2023_10_16_162232_create_token_conversion_histories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTokenConversionHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('token_conversion_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('token_conversions_id')->length(10)->unsigned()->nullable();
+            $table->integer('old_cash_value')->length(10)->unsigned()->nullable();
+            $table->integer('new_cash_value')->length(10)->unsigned()->nullable();
+            $table->integer('old_token_qty')->length(10)->unsigned()->nullable();
+            $table->integer('new_token_qty')->length(10)->unsigned()->nullable();
+            $table->integer('created_by')->length(10)->unsigned()->nullable();
+            $table->integer('updated_by')->length(10)->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('token_conversion_histories');
+    }
+}
