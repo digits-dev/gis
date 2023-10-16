244b211fffb06ac9835b49836a2bc60a6f70c28b
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-10-16 16:22:30 +0800

    locatons

---
+++ app/Models/Submaster/gasha_machines.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Submaster;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class gasha_machines extends Model
+{
+    use HasFactory;
+}
---
+++ database/seeders/Locations.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class Locations extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('locations')->insert([
+            [
+                'location_name' => 'Digits Head Office',
+                'status' => 'Active'
+            ],
+            [
+                'location_name' => 'Mitsukoshi',
+                'status' => 'Active'
+            ]
+          ]);
+    }
+}
