15e940fe20e79ab66d5f156e70700fbffa939021
unknown <gabrielcameronequing@digits.ph>
2023-10-17 11:17:01 +0800

    add Receiving Tokens

---
+++ app/Models/Token/ReceivingToken.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Token;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReceivingToken extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
