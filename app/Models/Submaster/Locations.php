f35ef2805fc3b10ec6f0532f6d97bf7b13757e43
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-10-17 12:04:28 +0800

    status seed

--- app/Models/Submaster/Locations.php
+++ app/Models/Submaster/Locations.php
@@ -10,7 +10,7 @@ class Locations extends Model
     use HasFactory;
     protected $table = 'locations';
 
-    public function scopeActive($query){
+    public function scopeDatas($query){
         return $query->where('status','ACTIVE')->get();
     }
 }
