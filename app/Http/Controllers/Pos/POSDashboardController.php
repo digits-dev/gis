9b1207f73ddd56bb408793dbacecf409b66ece59
patrickDigits <patrickpunzalan@digits.ph>
2023-10-18 16:10:17 +0800

    POS Frontend

--- app/Http/Controllers/Pos/POSDashboardController.php
+++ app/Http/Controllers/Pos/POSDashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Pos;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class POSDashboardController extends Controller
 {
@@ -13,7 +14,8 @@ class POSDashboardController extends Controller
      */
     public function index()
     {
-        //
+        
+        return view('Pos Frontend.views.dashboard');
     }
 
     /**
---
+++ resources/views/Pos Frontend/assets/sidebar.blade.php
@@ -0,0 +1,54 @@
+<section class="sidebar_section">
+    <nav>
+        <ul>
+            <li>
+                <a href="" class="gashapon_logo">
+                    <img src="{{ asset('img/logo.png') }}" alt="">
+                    <span class ="nav-item">GASHAPON</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-home"></i>
+                    <span class ="nav-item">Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa-solid fa-coins"></i>
+                    <span class ="nav-item">Token Swap</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-window-restore"></i>
+                    <span class ="nav-item">Swap History</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-money"></i>
+                    <span class ="nav-item">Float History</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-gear"></i>
+                    <span class ="nav-item">Setting</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa-solid fa-calendar"></i>
+                    <span class ="nav-item">End of Day</span>
+                </a>
+            </li>
+            {{-- <li>
+                <a href="" class="log-out">
+                    <i class="fa fa-sign-out"></i>
+                    <a  href="{{ route('logout') }}" class ="nav-item">Logout</a>
+                </a>
+            </li> --}}
+        </ul>
+    </nav>
+</section>
