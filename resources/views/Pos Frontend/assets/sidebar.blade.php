1c0bc0aac2bc81127bcc0bd7c9810e98fe1bbd9b
patrickDigits <patrickpunzalan@digits.ph>
2023-10-19 08:56:25 +0800

    pos dashboard

--- resources/views/Pos Frontend/assets/sidebar.blade.php
+++ resources/views/Pos Frontend/assets/sidebar.blade.php
@@ -2,53 +2,47 @@
     <nav>
         <ul>
             <li>
-                <a href="" class="gashapon_logo">
+                <a href="" class="gashapon_logo nav-title">
                     <img src="{{ asset('img/logo.png') }}" alt="">
                     <span class ="nav-item">GASHAPON</span>
                 </a>
             </li>
             <li>
-                <a href="">
+                <a class="nav-module" href="">
                     <i class="fa fa-home"></i>
                     <span class ="nav-item">Home</span>
                 </a>
             </li>
             <li>
-                <a href="">
+                <a class="nav-module" href="">
                     <i class="fa-solid fa-coins"></i>
                     <span class ="nav-item">Token Swap</span>
                 </a>
             </li>
             <li>
-                <a href="">
+                <a class="nav-module" href="">
                     <i class="fa fa-window-restore"></i>
                     <span class ="nav-item">Swap History</span>
                 </a>
             </li>
             <li>
-                <a href="">
+                <a class="nav-module" href="">
                     <i class="fa fa-money"></i>
                     <span class ="nav-item">Float History</span>
                 </a>
             </li>
             <li>
-                <a href="">
+                <a class="nav-module" href="">
                     <i class="fa fa-gear"></i>
                     <span class ="nav-item">Setting</span>
                 </a>
             </li>
             <li>
-                <a href="">
+                <a class="nav-module" href="">
                     <i class="fa-solid fa-calendar"></i>
                     <span class ="nav-item">End of Day</span>
                 </a>
             </li>
-            {{-- <li>
-                <a href="" class="log-out">
-                    <i class="fa fa-sign-out"></i>
-                    <a  href="{{ route('logout') }}" class ="nav-item">Logout</a>
-                </a>
-            </li> --}}
         </ul>
     </nav>
 </section>
--- resources/views/Pos Frontend/views/dashboard.blade.php
+++ resources/views/Pos Frontend/views/dashboard.blade.php
@@ -9,5 +9,25 @@
         @include('Pos Frontend.assets.sidebar')
         @include('Pos Frontend.assets.content')
     </section>
+
+    <script>
+
+        const userIcon = document.getElementById('user-icon');
+        const userIconDropdown = document.getElementById('user-icon-dropdown');
+
+
+        userIcon.addEventListener('click', function(event){
+            userIconDropdown.classList.toggle('show');  
+            event.stopPropagation();
+        });
+
+        document.addEventListener('click', function(event) {
+            // Check if the click target is not inside the userIconDropdown
+            if (!userIconDropdown.contains(event.target) && !userIcon.contains(event.target)) {
+                userIconDropdown.classList.remove('show');
+            }
+        });
+
+    </script>
 </body>
 </html>
