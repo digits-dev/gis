c51f49d882434725a7dad13b01d999066cc4c6ac
Fillinor <fillinorgunio@digits.ph>
2023-10-20 14:40:54 +0800

    add camera button and bar-code scanner for capsule refill

--- resources/views/capsule/capsule-refill.blade.php
+++ resources/views/capsule/capsule-refill.blade.php
@@ -1,155 +1,117 @@
-{{-- <!-- First, extends to the CRUDBooster Layout -->
 @extends('crudbooster::admin_template')
+@push('head')
+   <link rel="stylesheet" href="{{ asset('css/capsule-refill.css') }}">
+   <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+   <script src="{{ asset('js/code-scanner.js') }}"></script>
+   <script src="{{ asset('plugins/sweetalert.js') }}"></script>
+@endpush
 @section('content')
-<style>
-
-    .panel-content{
-        height: 500px;
-        display: flex;
-        justify-content: center;
-        align-content: center;
-        align-items: center;
-    }
-    .panel-default{
-        border-radius: 20px;
-        width: 400px;
-        margin-top: 100px;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
-    }
-    .panel-header{
-        /* background-color: green;  */
-        text-align:center;
-        margin-bottom: 10px;
-        font-size: 25px;
-        padding: 20px;
-    }
-    .panel-footer{
-        border-radius: 0px 0px 20px 20px;
-        text-align: center;
-    }
-    .form-group{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .form-group input{
-        width: 250px;
-        border-radius: 10px;
-        margin-bottom: 20px;
-    }
-    .panel-body{
-        height: 400px;
-    }
-    .panel-img{
-        text-align: center;
-    }
-    .panel-img img{
-        width: 250px;
-    }
-</style>
-    <!-- Your html goes here -->
-    <div class="panel-content">
-        <div class='panel panel-default'>
-            <div class='panel-header'>
-                <label>CAPSULE RETURN</label>
-            </div>
-            <div class='panel-body'>
-            <form method='post' action='{{CRUDBooster::mainpath('add-save')}}'>
-                <div class='form-group'>
-                <label>From Gasha Machine</label>
-                <input type='text' name='label1' required class='form-control'/>
-                <label>To Stockroom</label>
-                <input type='text' name='label1' required class='form-control'/>
-                <label>Quantity</label>
-                <input type='text' name='label1' required class='form-control'/>
-                </div>
-                <div class='panel-img'>
-                    <img src="{{ asset('img/capsule-refill.png') }}">
-                </div>
-                                
-            </form>
-            </div>
-            <div class='panel-footer'>
-            <input type='submit' class='btn btn-primary' value='Save changes'/>
-            </div>
-        </div>
-    </div>
-@endsection --}}
-
-<!-- First, extends to the CRUDBooster Layout -->
-@extends('crudbooster::admin_template')
-@section('content')
-<style>
-
-    .panel-content{
-        height: 500px;
-        display: flex;
-        justify-content: center;
-        align-content: center;
-        align-items: center;
-    }
-    .panel-default{
-        border-radius: 20px;
-        width: 400px;
-        margin-top: 100px;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
-    }
-    .panel-header{
-        /* background-color: green;  */
-        text-align:center;
-        margin-bottom: 10px;
-        font-size: 25px;
-        padding: 20px;
-    }
-    .panel-footer{
-        border-radius: 0px 0px 20px 20px;
-        text-align: center;
-    }
-    .form-group{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .form-group input{
-        width: 250px;
-        border-radius: 10px;
-        margin-bottom: 20px;
-    }
-    .panel-body{
-        height: 400px;
-    }
-    .panel-img{
-        text-align: center;
-    }
-    .panel-img img{
-        width: 250px;
-    }
-</style>
-    <!-- Your html goes here -->
     <div class="panel-content">
         <div class='panel panel-default'>
             <div class='panel-header'>
                 <label>CAPSULE REFILL</label>
             </div>
             <div class='panel-body'>
-            <form method='post' action='{{CRUDBooster::mainpath('add-save')}}'>
-                <div class='form-group'>
-                <label>Capsule Barcode</label>
-                <input type='text' name='label1' required class='form-control'/>
-                <label>To Gasha Machine</label>
-                <input type='text' name='label1' required class='form-control'/>
-                <label>Quantity</label>
-                <input type='text' name='label1' required class='form-control'/>
+                <div id="reader-wrapper" style="z-index: -3">
+                    <div id="reader"></div>
                 </div>
-                <div class='panel-img'>
-                    <img src="{{ asset('img/capsule-refill.png') }}">
-                </div>
-                                
-            </form>
+                <form method='post' action='{{CRUDBooster::mainpath('add-save')}}'>
+                    <div class='form-group'>
+                        <label>Capsule Barcode</label>
+                        <div class="flex input-btn">
+                            <input input-for="capsule" type='text' name='label1' required class='form-control'/>
+                            <button btn-for="capsule" type="button" class="btn btn-primary open-camera"><i class="fa fa-camera"></i></button>
+                        </div>
+                        <label>To Gasha Machine</label>
+                        <div class="flex input-btn">
+                            <input input-for="machine" type='text' name='label1' required class='form-control'/>
+                            <button btn-for="machine" type="button" class="btn btn-primary open-camera"><i class="fa fa-camera"></i></button>
+                        </div>
+                        <label>Quantity</label>
+                        <input type='text' name='label1' required class='form-control' id="quantity"/>
+                    </div>
+                    <div class='panel-img'>
+                        <img src="{{ asset('img/capsule-refill.png') }}">
+                    </div>
+                                    
+                </form>
             </div>
             <div class='panel-footer'>
-            <input type='submit' class='btn btn-primary' value='Save changes'/>
+            <button class="btn btn-primary" id="save-btn">Save Changes</button>
             </div>
         </div>
     </div>
 @endsection
+@push('bottom')
+<script>
+    let selectedCameraId = null;
+    let selectedInput = null;
+    let html5QrCode = null;
+    async function showCameraOptions(cameras) {
+        let cameraOptions = {}
+        cameras.forEach(camera => cameraOptions[camera.id] = camera.label);
+        const { value: fruit } = await Swal.fire({
+            title: 'Select a camera',
+            input: 'select',
+            inputOptions: cameraOptions,
+            inputPlaceholder: 'Select a camera',
+            showCancelButton: true,
+            inputValidator: (value) => {
+                selectedCameraId = value;
+                openVideo(selectedCameraId);
+            }
+        });
+
+    }
+
+    function openVideo(cameraId) {
+        if (html5QrCode) html5QrCode.stop();
+        html5QrCode = new Html5Qrcode("reader");
+        html5QrCode.start(
+        cameraId, 
+        {
+            fps: 10,
+            qrbox: { width: 250, height: 250 }
+        },
+        (decodedText, decodedResult) => {
+            html5QrCode.stop();
+            html5QrCode = null;
+            populateInput(decodedText);
+            $('#reader-wrapper').css('z-index', '-3')
+            
+        },
+        (errorMessage) => {
+            // parse error, ignore it.
+        })
+        .catch((err) => {
+        // Start failed, handle it.
+        });
+
+        $('#reader-wrapper').css('z-index', '1')
+    }
+
+    function populateInput(text) {
+        $(`input[input-for="${selectedInput}"]`).val(text);
+    }
+
+    $('.open-camera').on('click', function() {
+        selectedInput = $(this).attr('btn-for');
+        console.log(selectedInput);
+        if (selectedCameraId) {
+            openVideo(selectedCameraId);
+            return;
+        } 
+        Html5Qrcode.getCameras().then(devices => {
+            if (devices && devices.length) {
+                console.log(devices);
+                if (!selectedCameraId) {
+                    showCameraOptions(devices);
+                }
+            }
+        }).catch(err => {
+            alert('no cameras detected');
+        });
+    });
+
+</script>
+@endpush
