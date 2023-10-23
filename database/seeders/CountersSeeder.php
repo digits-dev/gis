dea87b697736b211dab8359b689a5a489c420106
Fillinor <fillinorgunio@digits.ph>
2023-10-23 09:51:39 +0800

    add capsule refill reference number to code counter seeder

--- database/seeders/CountersSeeder.php
+++ database/seeders/CountersSeeder.php
@@ -40,6 +40,14 @@ class CountersSeeder extends Seeder
                 'created_by'       => CRUDBooster::myId(),
                 'created_at'       => date('Y-m-d H:i:s')
             ],
+            [   
+                'cms_moduls_id'    => '33',
+                'reference_code'   => 'CR',
+                'reference_number' => '1',
+                'status'           => 'ACTIVE',
+                'created_by'       => CRUDBooster::myId(),
+                'created_at'       => date('Y-m-d H:i:s')
+            ],
         ];
 
         foreach ($counters as $counter) {
