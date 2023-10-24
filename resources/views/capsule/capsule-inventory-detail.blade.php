d00b855037e32a378cfec8fff91721b698462156
Fillinor <fillinorgunio@digits.ph>
2023-10-24 15:26:19 +0800

    show details page of capsule inventory

---
+++ resources/views/capsule/capsule-inventory-detail.blade.php
@@ -0,0 +1,56 @@
+@push('head')
+<style>
+    .panel-body {
+        background-color: #fff;
+    }
+</style>
+@endpush
+
+@extends('crudbooster::admin_template')
+@section('content')
+<p class="noprint">
+    <a title='Return' href="{{ CRUDBooster::mainPath() }}">
+        <i class='fa fa-chevron-circle-left '></i> &nbsp; {{trans("crudbooster.form_back_to_list",['module'=>CRUDBooster::getCurrentModule()->name])}}
+    </a>
+</p>
+<div class="panel-default">
+    <div class="panel-heading">
+        <i class="fa fa-eye"></i><strong> {{CRUDBooster::getCurrentModule()->name}}</strong>
+    </div>
+    <div class="panel-body">
+        <div class="table table-responsive">
+            <table class=" table table-striped">
+                <thead>
+                    <tr>
+                        <th>Item Code</th>
+                        <th>Item Description</th>
+                        <th>Location</th>
+                        <th>From</th>
+                        <th>Qty</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($lines as $line)
+                    <tr>
+                        <td>{{ $item->item_code }}</td>
+                        <td>{{ $item->item_description }}</td>
+                        <td>{{ $item->location_name }}</td>
+                        <td>{{ $line->sub_location ?? $line->serial_number }}</td>
+                        <td>{{ $line->qty }}</td>
+                    </tr>
+                    @endforeach
+                    <tr>
+                        <td colspan="3"></td>
+                        <td class="text-bold">Total</td>
+                        <td class="text-bold">{{ $item->onhand_qty }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="panel-footer">
+        <a href='{{ CRUDBooster::mainpath() }}' class='btn btn-default'>Back</a>
+    </div>
+</div>
+
+@endsection
