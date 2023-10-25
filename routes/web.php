f77291f3967e2e0c93e8401a864515b77ae26c4d
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-10-25 15:41:58 +0800

    done collect token

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::group(['middleware' => ['web']], function() {
     Route::post(config('crudbooster.ADMIN_PATH').'/add-collect-token/get-options-machines',[AdminCollectRrTokensController::class, 'getOptionMachines'])->name('get-options-machines');
     Route::get(config('crudbooster.ADMIN_PATH').'/collect_rr_tokens_receiving/get-edit/{id}', [AdminCollectRrTokensReceivingController::class, 'getEdit']); 
     Route::post(config('crudbooster.ADMIN_PATH').'/collect_rr_tokens/get-machine', [AdminCollectRrTokensController::class, 'getMachine'])->name('get_machine');
-
+    Route::post(config('crudbooster.ADMIN_PATH').'/collect_rr_tokens/check-inventory-qty',[AdminCollectRrTokensController::class, 'checkInventoryQty'])->name('check-inventory-qty');
     //Pullout
     Route::get(config('crudbooster.ADMIN_PATH').'/pullout_tokens/getPulloutForPrint/{id}',[AdminPulloutTokensController::class, 'getPulloutForPrint'])->name('pullout-for-print');
     Route::get(config('crudbooster.ADMIN_PATH').'/pullout_tokens/forPrintPulloutUpdate',[AdminPulloutTokensController::class, 'forPrintPulloutUpdate']);
