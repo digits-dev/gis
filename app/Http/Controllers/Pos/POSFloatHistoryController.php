32359262743d712cf47a36fbfad34dba7403b741
unknown <gabrielcameronequing@digits.ph>
2023-10-25 12:11:20 +0800

    Working on the POS Float History

--- app/Http/Controllers/Pos/POSFloatHistoryController.php
+++ app/Http/Controllers/Pos/POSFloatHistoryController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers\Pos;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Submaster\FloatEntry;
+use App\Models\Submaster\FloatType;
+use App\Models\Submaster\CashFloatHistory;
+use App\Models\Submaster\ModeOfPayment;
+use App\Models\Submaster\CashFloatHistoryLine;
+use DB;
 
 class POSFloatHistoryController extends Controller
 {
@@ -15,7 +21,36 @@ class POSFloatHistoryController extends Controller
     public function index()
     {
         $data = [];
+
         
+        $cash_floats_histories_id = CashFloatHistory::pluck('id');
+
+
+        $data['entry_token'] = DB::table('cash_float_histories')
+            ->leftJoin('cash_float_history_lines as cfhl', 'cfhl.cash_float_histories_id', 'cash_float_histories.id')
+            ->leftJoin('float_types as ft', 'ft.id', 'cash_float_histories.float_types_id')
+            ->leftJoin('float_entries as fe', 'fe.id', 'cfhl.float_entries_id')
+            // ->select('cash_float_histories.*','cfhl.*','ft.*','fe.*')
+            // ->where('fe.description','TOKEN')
+            ->select('cash_float_histories.id',
+                'fe.description as fe_description',
+                'cash_float_histories.created_at',
+                'cash_float_histories.created_by',
+                'cfhl.value as cfhl_value',
+                'ft.description as ft_description')
+            ->get();
+            // ->pluck('cfhl.value')
+            // ->toArray();
+
+        // $data['entry_token'] = DB::table('cash_float_history_lines')
+        //     ->leftJoin('cash_float_histories as cfh', 'cfh.id', 'cash_float_history_lines.cash_float_histories_id')
+        //     ->leftJoin('float_types as ft', 'ft.id', 'cfh.float_types_id')
+        //     ->select('cash_float_history_lines.*', 'cfh.*','ft.*')
+        //     ->get();
+
+
+        dd($data['entry_token']);
+        // $data['entry_id'] = CashFloatHistory::get();
         return view('pos-frontend.views.float-history', $data);
     }
 
