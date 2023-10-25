25f409d6f179f2d676e17db632df1de4ac2ac57a
Fillinor <fillinorgunio@digits.ph>
2023-10-25 13:24:04 +0800

    show ref number on swal refill

--- resources/views/capsule/capsule-refill.blade.php
+++ resources/views/capsule/capsule-refill.blade.php
@@ -182,7 +182,6 @@
             
             Swal.fire({
                 title: `No. of tokens mismatched.`,
-                // html: `${data.item.digits_code} is worth ${data.item.no_of_tokens} tokens and ${data.machine.serial_number} accepts ${data.machine.no_of_token} tokens.`,
                 html: outerHTML,
                 icon: 'error',
                 returnFocus: false,
@@ -202,6 +201,7 @@
         } else {
             Swal.fire({
                 title: 'Machine successfully refilled.',
+                html: `<strong>Ref #: ${data.reference_number} </strong>`,
                 icon: 'success',
                 returnFocus: false,
             }).then(() => {
