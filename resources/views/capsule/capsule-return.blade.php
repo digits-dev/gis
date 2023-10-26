dfd0238f0128ca84d7defb17b04f4ce61af86f5e
patrickDigits <patrickpunzalan@digits.ph>
2023-10-26 17:10:12 +0800

    prevent close outside

--- resources/views/capsule/capsule-return.blade.php
+++ resources/views/capsule/capsule-return.blade.php
@@ -525,6 +525,7 @@
                                 // confirmButtonText: 'Save',
                                 showConfirmButton: false,
                                 showCloseButton: true,
+                                allowOutsideClick: false,
                                 returnFocus: false,
                                 html: swal_content,
                             })
