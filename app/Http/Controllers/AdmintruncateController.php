3d90b658b096629934ff59f341a0be1e542d1994
patrickDigits <patrickpunzalan@digits.ph>
2023-10-27 10:49:43 +0800

    update truncate endpoint

--- app/Http/Controllers/AdmintruncateController.php
+++ app/Http/Controllers/AdmintruncateController.php
@@ -20,6 +20,17 @@ class AdminTruncateController extends \crocodicstudio\crudbooster\controllers\CB
         DB::table('token_inventories')->truncate();
         DB::table('collect_rr_tokens')->truncate();
         DB::table('collect_rr_token_lines')->truncate();
+        DB::table('capsule_refills')->truncate();
+        DB::table('capsule_returns')->truncate();
+        DB::table('capsule_sales')->truncate();
+        DB::table('cash_float_histories')->truncate();
+        DB::table('cash_float_history_lines')->truncate();
+        DB::table('gasha_machines')->truncate();
+        DB::table('history_capsules')->truncate();
+        DB::table('inventory_capsules')->truncate();
+        DB::table('inventory_capsule_lines')->truncate();
+        DB::table('swap_histories')->truncate();
+        DB::table('token_conversion_histories')->truncate();
 
         return "Truncated Successfully";
     }
