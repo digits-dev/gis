6a6f4bcbbab1862a121d5ff7b94c6a9c76ce3837
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-11-03 16:57:38 +0800

    cms users isd

--- app/Http/Controllers/AdminCmsUsersController.php
+++ app/Http/Controllers/AdminCmsUsersController.php
@@ -17,6 +17,7 @@ class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CB
 		$this->button_action_style = 'button_icon';	
 		$this->button_import 	   = FALSE;	
 		$this->button_export 	   = FALSE;	
+		$this->button_delete 	   = FALSE;
 		# END CONFIGURATION DO NOT REMOVE THIS LINE
 	
 		# START COLUMNS DO NOT REMOVE THIS LINE
@@ -33,8 +34,8 @@ class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CB
 		$this->form = array(); 		
 		$this->form[] = array("label"=>"Name","name"=>"name",'required'=>true,'validation'=>'required|alpha_spaces|min:3','width'=>'col-sm-5');
 		$this->form[] = array("label"=>"Email","name"=>"email",'required'=>true,'type'=>'email','validation'=>'required|email|unique:cms_users,email,'.CRUDBooster::getCurrentId(),'width'=>'col-sm-5');		
-		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'required'=>true,'validation'=>'required|image|max:1000','resize_width'=>90,'resize_height'=>90,'width'=>'col-sm-5');											
-		$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true,'width'=>'col-sm-5');						
+		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'validation'=>'image|max:1000','resize_width'=>90,'resize_height'=>90,'width'=>'col-sm-5');											
+		$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true,'width'=>'col-sm-5', 'datatable_where'=>"id not in (1)");						
 		$this->form[] = array("label"=>"Location","name"=>"location_id","type"=>"select2","datatable"=>"locations,location_name", 'datatable_where'=>"status = 'ACTIVE'",'width'=>'col-sm-5');
 		// $this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
 		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change",'width'=>'col-sm-5');
@@ -127,6 +128,14 @@ class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CB
 
         return $this->view('crudbooster::default.form',$data);
 	}
+
+	public function hook_query_index(&$query) {
+        if(CRUDBooster::myPrivilegeId() == 9) {
+			$query->where('cms_users.id_cms_privileges','!=','1');
+        }else{
+			$query->orderBy('cms_users.id', 'desc');
+		} 
+    }
 
 	public function hook_before_add(&$postdata) {      
 	    unset($postdata['password_confirmation']);
