c07c17387515d54babc13148645139fdbadc188c
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-11-15 12:47:25 +0800

    fix modulo bt zero collect token view

--- resources/views/audit/collect-token/detail-collect-token.blade.php
+++ resources/views/audit/collect-token/detail-collect-token.blade.php
@@ -135,7 +135,7 @@
                                 <th width="10%" class="text-center">Current cash value</th>
                             </tr>      
                         @foreach($detail_body as $row)
-                            @if($row->qty % $row->no_of_token === 0)
+                            @if($row->qty % (isset($row->no_of_token_line) ? $row->no_of_token_line : 1) === 0)
                                 <tr>
                                     <td style="text-align:center" height="10">
                                         {{$row->serial_number}}                               
