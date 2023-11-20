389fab094b79d51fec5435fdd4bbbc7a146ee0a9
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-11-20 19:21:46 +0800

    fix bugs conflict merge

--- app/Http/Controllers/Submaster/AdminSubLocationController.php
+++ app/Http/Controllers/Submaster/AdminSubLocationController.php
@@ -42,11 +42,7 @@
 			# START FORM DO NOT REMOVE THIS LINE
 			$this->form = [];
 			$this->form[] = ['label'=>'Location','name'=>'location_id','type'=>'select2','validation'=>'required|integer|min:0','width'=>'col-sm-10','datatable'=>'locations,location_name'];
-<<<<<<< Updated upstream
-			$this->form[] = ['label'=>'Sub Location','name'=>'description','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
-=======
 			$this->form[] = ['label'=>'Sub Location','name'=>'desription','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
->>>>>>> Stashed changes
 			if(in_array(CRUDBooster::getCurrentMethod(), ['getEdit','getDetail','postEditSave'])){
 				$this->form[] = ['label'=>'Status','name'=>'status','type'=>'select2','validation'=>'required','width'=>'col-sm-10','dataenum'=>'ACTIVE;INACTIVE'];			# END FORM DO NOT REMOVE THIS LINE
 			}
