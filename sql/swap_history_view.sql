d662258112a5c3def189912b95dd823ba26eae91
Fillinor <fillinorgunio@digits.ph>
2023-11-20 11:19:28 +0800

    update query for swap_history_view

--- sql/swap_history_view.sql
+++ sql/swap_history_view.sql
@@ -1,10 +1,12 @@
 SELECT
-    mop.id AS mode_of_payments_id,
     date_table.formatted_date,
+    mop.id AS mode_of_payments_id,
     COALESCE(
         SUM(swap_histories.total_value),
         0
-    ) AS total_value
+    ) AS total_value,
+    swap_histories.created_by,
+    swap_histories.locations_id
 FROM (
         SELECT id
         FROM
@@ -16,14 +18,21 @@ FROM (
         FROM
             swap_histories
     ) AS date_table
-    LEFT JOIN swap_histories ON mop.id = swap_histories.mode_of_payments_id
+    LEFT JOIN (
+        SELECT *
+        FROM swap_histories
+        WHERE
+            swap_histories.status = 'POSTED'
+    ) AS swap_histories ON mop.id = swap_histories.mode_of_payments_id
     AND date_table.formatted_date = DATE_FORMAT(
         swap_histories.created_at,
         '%Y-%m-%d'
     )
 GROUP BY
     mop.id,
-    date_table.formatted_date
+    date_table.formatted_date,
+    swap_histories.locations_id,
+    swap_histories.created_by
 ORDER BY
-    mop.id,
-    date_table.formatted_date;
+    date_table.formatted_date,
+    mop.id;
