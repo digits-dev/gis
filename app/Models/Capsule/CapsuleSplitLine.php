2cabc3fb8b1e395cfe534096e0d348fb21a90161
Fillinor <fillinorgunio@digits.ph>
2023-11-24 11:54:40 +0800

    create model for capsule split lines

---
+++ app/Models/Capsule/CapsuleSplitLine.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Capsule;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CapsuleSplitLine extends Model
+{
+    use HasFactory;
+    protected $table = 'capsule_split_lines';
+}
