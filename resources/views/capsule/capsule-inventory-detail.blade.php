889b69c390ca45251f59573e79bd54f3913e4ef9
Fillinor <fillinorgunio@digits.ph>
2023-11-28 15:06:13 +0800

    update ui for capsule inventory details

--- resources/views/capsule/capsule-inventory-detail.blade.php
+++ resources/views/capsule/capsule-inventory-detail.blade.php
@@ -1,57 +1,110 @@
+
+
 @push('head')
-<style>
-    .panel-body {
-        background-color: #fff;
+<script src="{{ asset('plugins/sweetalert.js') }}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<style type="text/css">   
+    .select2-selection__choice{
+            font-size:14px !important;
+            color:black !important;
     }
+    .select2-selection__rendered {
+        line-height: 31px !important;
+    }
+    .select2-container .select2-selection--single {
+        height: 35px !important;
+    }
+    .select2-selection__arrow {
+        height: 34px !important;
+    }
+
+    th, td {
+        border: 1px solid rgba(000, 0, 0, .5) !important;
+        padding: 8px;
+    }
+
+
+    @keyframes infinite-spinning {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+
+    @media (min-width:729px){
+        .panel-default{
+            width:60% !important; 
+            margin:auto !important;
+        }
+    }
+
 </style>
 @endpush
-
 @extends('crudbooster::admin_template')
 @section('content')
-<p class="noprint">
-    <a title='Return' href="{{ CRUDBooster::mainPath() }}">
-        <i class='fa fa-chevron-circle-left '></i> &nbsp; {{trans("crudbooster.form_back_to_list",['module'=>CRUDBooster::getCurrentModule()->name])}}
-    </a>
-</p>
-<div class="panel-default">
-    <div class="panel-heading">
-        <i class="fa fa-eye"></i><strong> {{CRUDBooster::getCurrentModule()->name}}</strong>
-    </div>
-    <div class="panel-body">
-        <div class="table table-responsive">
-            <table class=" table table-striped">
+@if(g('return_url'))
+<p class="noprint"><a title='Return' href='{{g("return_url")}}'><i class='fa fa-chevron-circle-left '></i> &nbsp; {{trans("crudbooster.form_back_to_list",['module'=>CRUDBooster::getCurrentModule()->name])}}</a></p>       
+@else
+<p class="noprint"><a title='Main Module' href='{{CRUDBooster::mainpath()}}'><i class='fa fa-chevron-circle-left '></i> &nbsp; {{trans("crudbooster.form_back_to_list",['module'=>CRUDBooster::getCurrentModule()->name])}}</a></p>       
+@endif
+
+<div class='panel panel-default'>
+    <div class='panel-heading' style="background-color:#3c8dbc; color:#fff">Capsule Inventory Detail</div>
+    <div class='panel-body'>
+        <div class="col-md-12">
+            <div class="form-group">
+                <label class="control-label">JAN #</label>
+                <input type="text" class="form-control finput" value="{{ $item->digits_code }}" readonly>
+            </div>
+        
+            <div class="form-group">
+                <label class="control-label">Digits Code</label>
+                <input type="text" class="form-control finput" value="{{ $item->digits_code2 }}" readonly>
+            </div>
+
+            <div class="form-group">
+                <label class="require control-label">Item Description</label>
+                <input type="text" class="form-control finput" value="{{ $item->item_description }}" readonly>
+            </div>
+
+            <div class="form-group">
+                <label class="require control-label">Location</label>
+                <input type="text" class="form-control finput" value="{{ $item->location_name }}" readonly>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <table class="table" style="width: 100%">
                 <thead>
                     <tr>
-                        <th>JAN Number</th>
-                        <th>Digits Code</th>
-                        <th>Item Description</th>
-                        <th>Location</th>
-                        <th>From</th>
-                        <th>Qty</th>
-                    </tr>
+                        <th class="text-center">From</th>
+                        <th class="text-center">Qty</th>
+                    </tr>  
                 </thead>
-                <tbody>
+                            
+                <tbody id="bodyTable">    
                     @foreach ($lines as $line)
-                    <tr>
-                        <td>{{ $item->digits_code }}</td>
-                        <td>{{ $item->digits_code2 }}</td>
-                        <td>{{ $item->item_description }}</td>
-                        <td>{{ $item->location_name }}</td>
-                        <td>{{ $line->sub_location ?? $line->serial_number }}</td>
-                        <td>{{ $line->qty }}</td>
-                    </tr>
+                        <tr>
+                            <td class="text-center">
+                                {{$line->sub_location ?? $line->serial_number}}                               
+                            </td>
+                            <td class="text-center">
+                                {{$line->qty}}                               
+                            </td>
+                        </tr>
                     @endforeach
                     <tr>
-                        <td colspan="4"></td>
-                        <td class="text-bold">Total</td>
-                        <td class="text-bold">{{ $item->onhand_qty }}</td>
-                    </tr>
+                        <td class="text-center text-bold">Total</td>
+                        <td class="text-center text-bold">{{ $item->onhand_qty }}</td>
+                    </tr>                                         
                 </tbody>
             </table>
-        </div>
+        </div>   
     </div>
-    <div class="panel-footer">
-        <a href='{{ CRUDBooster::mainpath() }}' class='btn btn-default'>Back</a>
+    <div class='panel-footer'>
+        <a href="{{ CRUDBooster::mainpath() }}" class="btn btn-default">{{ trans('message.form.back') }}</a>
     </div>
 </div>
 
