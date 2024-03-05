0178eb4afc702a0e66505d7f548f931bcdb01ecb
DTCRepo <marvinmosico@digits.ph>
2024-03-05 15:30:38 +0800

    add migration to swap and add ons

---
+++ database/migrations/2024_03_05_152811_add_uuid_to_add_ons.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUuidToAddOns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('add_ons', function (Blueprint $table) {
+            $table->integer('uuid')->length(10)->unsigned()->nullable()->after('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('add_ons', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+}
