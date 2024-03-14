8d4bf078442d885082e9a67b17c555ad515e4397
Fillinor <fillinorgunio@digits.ph>
2024-03-14 11:35:10 +0800

    update inventory_capsule_view

---
+++ database/migrations/2024_03_14_113135_update_inventory_capsule_view.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateInventoryCapsuleView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("DROP VIEW IF EXISTS inventory_capsule_view");
+        DB::statement("
+            CREATE VIEW inventory_capsule_view AS
+            select
+                inventory_capsules.id as inventory_capsules_id,
+                coalesce(stock_room_qty.qty, 0) as stockroom_capsule_qty,
+                coalesce(machine_qty.qty, 0) as machine_capsule_qty,
+                coalesce(stock_room_qty.qty, 0) + coalesce(machine_qty.qty, 0) as onhand_qty
+            from inventory_capsules
+                left JOIN (
+                    select
+                        inventory_capsule_lines.inventory_capsules_id,
+                        SUM(inventory_capsule_lines.qty) as qty
+                    from
+                        inventory_capsule_lines as inventory_capsule_lines
+                    where
+                        inventory_capsule_lines.sub_locations_id is not null
+                    group by inventory_capsule_lines.inventory_capsules_id
+                ) as stock_room_qty on stock_room_qty.inventory_capsules_id = inventory_capsules.id
+                left join (
+                    select
+                        inventory_capsule_lines.inventory_capsules_id,
+                        SUM(inventory_capsule_lines.qty) as qty
+                    from
+                        inventory_capsule_lines as inventory_capsule_lines
+                    where
+                        inventory_capsule_lines.gasha_machines_id is not null
+                    group by inventory_capsule_lines.inventory_capsules_id
+                ) as machine_qty on machine_qty.inventory_capsules_id = inventory_capsules.id       
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- sql/inventory_capsule_view.sql
+++ sql/inventory_capsule_view.sql
@@ -1,33 +1,26 @@
 select
     inventory_capsules.id as inventory_capsules_id,
-    consolidation.stockroom_capsule_qty,
-    consolidation.machine_capsule_qty,
-    consolidation.stockroom_capsule_qty + consolidation.machine_capsule_qty as onhand_qty
+    coalesce(stock_room_qty.qty, 0) as stockroom_capsule_qty,
+    coalesce(machine_qty.qty, 0) as machine_capsule_qty,
+    coalesce(stock_room_qty.qty, 0) + coalesce(machine_qty.qty, 0) as onhand_qty
 from inventory_capsules
     left JOIN (
         select
             inventory_capsule_lines.inventory_capsules_id,
-            COALESCE(
-                inventory_capsule_lines.qty,
-                0
-            ) as stockroom_capsule_qty,
-            COALESCE(
-                subquery.machine_capsule_qty,
-                0
-            ) as machine_capsule_qty
+            SUM(inventory_capsule_lines.qty) as qty
         from
-            inventory_capsule_lines
-            left join (
-                select
-                    inner_query.inventory_capsules_id,
-                    SUM(qty) as machine_capsule_qty
-                from
-                    inventory_capsule_lines as inner_query
-                where
-                    inner_query.gasha_machines_id is not null
-                GROUP BY
-                    inner_query.inventory_capsules_id
-            ) as subquery on subquery.inventory_capsules_id = inventory_capsule_lines.inventory_capsules_id
+            inventory_capsule_lines as inventory_capsule_lines
         where
             inventory_capsule_lines.sub_locations_id is not null
-    ) as consolidation on consolidation.inventory_capsules_id = inventory_capsules.id
+        group by inventory_capsule_lines.inventory_capsules_id
+    ) as stock_room_qty on stock_room_qty.inventory_capsules_id = inventory_capsules.id
+    left join (
+        select
+            inventory_capsule_lines.inventory_capsules_id,
+            SUM(inventory_capsule_lines.qty) as qty
+        from
+            inventory_capsule_lines as inventory_capsule_lines
+        where
+            inventory_capsule_lines.gasha_machines_id is not null
+        group by inventory_capsule_lines.inventory_capsules_id
+    ) as machine_qty on machine_qty.inventory_capsules_id = inventory_capsules.id
