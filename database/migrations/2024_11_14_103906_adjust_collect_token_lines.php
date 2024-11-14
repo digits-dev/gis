e18e0dffd9454e46a8cba78f974a01e87e7a5685
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2024-11-14 11:14:14 +0800

    update: add migrations

---
+++ database/migrations/2024_11_14_103906_adjust_collect_token_lines.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AdjustCollectTokenLines extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('collect_rr_token_lines', function (Blueprint $table) {
+
+            $table->integer('variance_type')->after('variance')->nullable();
+            $table->integer('projected_capsule_sales')->after('variance_type')->nullable();
+            $table->integer('actual_capsule_sales')->after('projected_capsule_sales')->nullable();
+            $table->integer('current_capsule_inventory')->after('actual_capsule_sales')->nullable();
+            $table->integer('actual_capsule_inventory')->after('current_capsule_inventory')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('collect_rr_token_lines', function (Blueprint $table) {
+            $table->dropColumn('variance_type');
+            $table->dropColumn('projected_capsule_sales');
+            $table->dropColumn('actual_capsule_sales');
+            $table->dropColumn('current_capsule_inventory');
+            $table->dropColumn('actual_capsule_inventory');
+        });
+    }
+}
